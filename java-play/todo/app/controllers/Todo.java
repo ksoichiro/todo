35abdcfad3ce49b7a98dac76deabaf8793a28eb4
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-03-23 02:42:21 +0900

    [Java Play] Modified how to get username.

--- java-play/todo/app/controllers/Todo.java
+++ java-play/todo/app/controllers/Todo.java
@@ -1,6 +1,5 @@
 package controllers;
 
-import models.User;
 import play.data.Form;
 import play.mvc.Result;
 import play.mvc.Security;
@@ -8,23 +7,18 @@ import views.formdata.TodoForm;
 import views.html.todo;
 
 import static play.data.Form.form;
-import static play.mvc.Controller.request;
-import static play.mvc.Results.badRequest;
-import static play.mvc.Results.ok;
-import static play.mvc.Results.redirect;
+import static play.mvc.Results.*;
 
 @Security.Authenticated(Secured.class)
 public class Todo {
     public static Result index() {
-        return ok(todo.render(form(TodoForm.class).fill(new TodoForm()),
-                User.find.where().eq("username", request().username()).findUnique()));
+        return ok(todo.render(form(TodoForm.class).fill(new TodoForm())));
     }
 
     public static Result create() {
         Form<TodoForm> form = form(TodoForm.class).bindFromRequest();
         if (form.hasErrors()) {
-            return badRequest(todo.render(form,
-                    User.find.where().eq("username", request().username()).findUnique()));
+            return badRequest(todo.render(form));
         }
         return redirect(routes.Todo.index());
     }
