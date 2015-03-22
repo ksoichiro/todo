c40974addd0556e0113a6cb0cdc6889446087beb
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-03-23 02:09:14 +0900

    [Java Play] Added username after login.

--- java-play/todo/app/controllers/Todo.java
+++ java-play/todo/app/controllers/Todo.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.User;
 import play.data.Form;
 import play.mvc.Result;
 import play.mvc.Security;
@@ -7,6 +8,7 @@ import views.formdata.TodoForm;
 import views.html.todo;
 
 import static play.data.Form.form;
+import static play.mvc.Controller.request;
 import static play.mvc.Results.badRequest;
 import static play.mvc.Results.ok;
 import static play.mvc.Results.redirect;
@@ -14,13 +16,15 @@ import static play.mvc.Results.redirect;
 @Security.Authenticated(Secured.class)
 public class Todo {
     public static Result index() {
-        return ok(todo.render(form(TodoForm.class).fill(new TodoForm())));
+        return ok(todo.render(form(TodoForm.class).fill(new TodoForm()),
+                User.find.where().eq("username", request().username()).findUnique()));
     }
 
     public static Result create() {
         Form<TodoForm> form = form(TodoForm.class).bindFromRequest();
         if (form.hasErrors()) {
-            return badRequest(todo.render(form));
+            return badRequest(todo.render(form,
+                    User.find.where().eq("username", request().username()).findUnique()));
         }
         return redirect(routes.Todo.index());
     }
