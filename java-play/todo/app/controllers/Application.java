4c16e96cf3f55d452487b8ea9c1034632a6498af
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-03-23 01:59:06 +0900

    [Java Play] Modified user authentication.

--- java-play/todo/app/controllers/Application.java
+++ java-play/todo/app/controllers/Application.java
@@ -3,12 +3,14 @@ package controllers;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Security;
 import views.formdata.Login;
 import views.html.login;
 
 import static play.data.Form.form;
 
 public class Application extends Controller {
+    @Security.Authenticated(Secured.class)
     public static Result index() {
         return redirect(routes.Todo.index());
     }
@@ -27,6 +29,8 @@ public class Application extends Controller {
         if (form.hasErrors()) {
             return badRequest(login.render(form));
         }
+        session().clear();
+        session("username", form.get().username);
         return redirect(routes.Todo.index());
     }
 }
--- java-play/todo/app/controllers/Todo.java
+++ java-play/todo/app/controllers/Todo.java
@@ -2,6 +2,7 @@ package controllers;
 
 import play.data.Form;
 import play.mvc.Result;
+import play.mvc.Security;
 import views.formdata.TodoForm;
 import views.html.todo;
 
@@ -10,14 +11,16 @@ import static play.mvc.Results.badRequest;
 import static play.mvc.Results.ok;
 import static play.mvc.Results.redirect;
 
+@Security.Authenticated(Secured.class)
 public class Todo {
     public static Result index() {
         return ok(todo.render(form(TodoForm.class).fill(new TodoForm())));
     }
+
     public static Result create() {
-        Form<models.Todo> form = form(models.Todo.class).bindFromRequest();
+        Form<TodoForm> form = form(TodoForm.class).bindFromRequest();
         if (form.hasErrors()) {
-//            return badRequest(form);
+            return badRequest(todo.render(form));
         }
         return redirect(routes.Todo.index());
     }
