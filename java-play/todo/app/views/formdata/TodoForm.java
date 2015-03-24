041634915a40eb381cb6fb498dc62767de5e1359
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-03-25 01:49:24 +0900

    [Java Play] Added validation for creating todo.

--- java-play/todo/app/views/formdata/TodoForm.java
+++ java-play/todo/app/views/formdata/TodoForm.java
@@ -1,8 +1,15 @@
 package views.formdata;
 
+import play.data.validation.Constraints;
+
 public class TodoForm {
+    @Constraints.Required
+    @Constraints.MinLength(1)
+    @Constraints.MaxLength(1024)
     public String title;
 
+    @Constraints.MinLength(0)
+    @Constraints.MaxLength(1024)
     public String note;
 
     public String todoStateId;
