762495e345cc2b59639af13c6f220951fadd6056
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-04-03 00:32:25 +0900

    [Groovy Spring Boot] Replaced Principal to AuthenticationPrincipal.

--- groovy-spring-boot/todo/app-site/src/main/groovy/com/github/ksoichiro/todo/groovy/springboot/web/TodoController.groovy
+++ groovy-spring-boot/todo/app-site/src/main/groovy/com/github/ksoichiro/todo/groovy/springboot/web/TodoController.groovy
@@ -6,14 +6,12 @@ import com.github.ksoichiro.todo.groovy.springboot.form.TodoUpdateForm
 import com.github.ksoichiro.todo.groovy.springboot.service.TodoService
 import com.github.ksoichiro.todo.groovy.springboot.service.TodoStateService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.security.core.Authentication
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
-
-import java.security.Principal
 
 @Controller
 @RequestMapping("todos")
@@ -24,32 +22,24 @@ public class TodoController {
     TodoService todoService
 
     @RequestMapping
-    public String index(Principal principal, TodoForm form, Model model) {
+    public String index(@AuthenticationPrincipal User user, TodoForm form, Model model) {
         model.addAttribute("allTodoStates", todoStateService.findAll())
-
-        Authentication authentication = (Authentication) principal
-        User user = (User) authentication.getPrincipal()
         model.addAttribute("allTodos", todoService.findAll(user.id))
-
         "todos/index"
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String create(Principal principal, @Validated TodoForm form, BindingResult bindingResult, Model model) {
+    public String create(@AuthenticationPrincipal User user, @Validated TodoForm form, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
-            return index(principal, form, model)
+            return index(user, form, model)
         }
-        Authentication authentication = (Authentication) principal
-        User user = (User) authentication.getPrincipal()
         todoService.save(form, user.id)
         "redirect:/todos"
     }
 
     @RequestMapping(value = "/{id}/update", method = RequestMethod.POST, consumes = ["application/json;charset=UTF-8"], produces=["application/json;charset=UTF-8"])
     @ResponseBody
-    public String update(Principal principal, @RequestBody TodoUpdateForm form) {
-        Authentication authentication = (Authentication) principal
-        User user = (User) authentication.getPrincipal()
+    public String update(@AuthenticationPrincipal User user, @RequestBody TodoUpdateForm form) {
         if (todoService.ownedBy(form.id.toLong(), user.id)) {
             todoService.update(form)
         }
@@ -57,9 +47,7 @@ public class TodoController {
     }
 
     @RequestMapping(value = "/{id}/delete", method = RequestMethod.POST)
-    public String delete(Principal principal, TodoForm form, Model model, @PathVariable Long id) {
-        Authentication authentication = (Authentication) principal
-        User user = (User) authentication.getPrincipal()
+    public String delete(@AuthenticationPrincipal User user, TodoForm form, Model model, @PathVariable Long id) {
         if (todoService.ownedBy(id, user.id)) {
             todoService.delete(id)
         }
